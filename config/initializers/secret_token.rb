5bdfb85851f28d4bb76211948bf87228a5d82c9b
Yusuke Shiki <fxxker69@mac.com>
2012-09-09 02:20:47 +0900

    'test'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrivateRailsStudy::Application.config.secret_token = '7606fad471edf25a11c282a6e1afbd9cbb35cf115ece6efc93b09951e1757f14afa603dc6b7b51da50abda69e11d82536fef1dae0542223c6841e8e112d85279'
